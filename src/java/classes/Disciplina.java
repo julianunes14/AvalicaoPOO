d0bd78da3dd8ea5733ae8218be4404c1b3bf9fc1
Julia Nunes <nuneesjgl@gmail.com>
2020-10-03 23:45:07 -0300

    classe Disciplina criada

--- src/java/classes/Disciplina.java
+++ src/java/classes/Disciplina.java
@@ -5,10 +5,59 @@
  */
 package classes;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Júlia Nunes
  */
 public class Disciplina {
+    private String nome;
+    private String ementa;
+    private String ciclo;
+    private Double nota;
     
+    public Disciplina(String nome, String ementa, String ciclo){
+        this.nome = nome;
+        this.ciclo = ciclo;
+        this.ementa = ementa;
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmenta() {
+        return ementa;
+    }
+
+    public void setEmenta(String ementa) {
+        this.ementa = ementa;
+    }
+
+    public String getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(String ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+    public static ArrayList<Disciplina> getList(ArrayList lista) {
+        ArrayList<Disciplina> lstDisciplina = (ArrayList<Disciplina>)lista;
+        
+        return lstDisciplina;
+    }
 }
