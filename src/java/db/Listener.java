9ff0531673a1ff14b6d04815769542a935864bf2
Júlia Nunes <nuneesjgl@gmail.com>
2020-11-30 02:35:25 -0300

    Criando Listener

---
+++ src/java/db/Listener.java
@@ -0,0 +1,40 @@
+
+
+package db;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.sql.*;
+
+import fatec.Disciplina;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Listener implements ServletContextListener{
+    private static final String CLASS_NAME ="org.sqlite.JDBC";
+    private static final String DB_URL="jdbc:sqlite:/Sqlite/sqlitejava.db";
+    
+    
+    public static String exceptionMessage = null;
+    
+    public static Connection getConnection() throws Exception{
+        return DriverManager.getConnection(DB_URL);
+    }
+    
+    public void contextInitialized(ServletContextEvent sce){
+        Connection con = null;
+        Statement stmt =null;
+        
+        try{
+            Class.forName(CLASS_NAME);
+            con =getConnection();
+            stmt = con.createStatement();
+            stmt.execute(Disciplina.getCreateStatement());
+        } catch (Exception ex) {
+            exceptionMessage = ex.getLocalizedMessage();
+        }finally{
+            try{stmt.close();}catch(Exception ex){}
+            try{con.close();}catch(Exception ex){}
+        }
+    }
+    public void contextDestroyed(ServletContextEvent sce){}
+}
